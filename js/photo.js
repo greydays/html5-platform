1423c525ca551f45fdae09155d0981a63fa0e678
Ryan Flaherty <rcflaherty@gmail.com>
2015-06-12 12:24:16 -0700

    photobooth added, some formatting updated

--- js/photo.js
+++ js/photo.js
@@ -30,7 +30,7 @@ window.addEventListener("DOMContentLoaded", function() {
   }
   // Trigger photo take
   document.getElementById("snap").addEventListener("click", function() {
-    context.drawImage(video, 0, 0, 320, 240);
+    context.drawImage(video, 0, 0, 256, 192);
   });
 }, false);
 
